f6cb46538bb8301185d313b3aaf2e25f29690100
Alexey Rybakov <alexrybakov@yandex.ru>
2020-07-18 19:43:58 +0300

    Trending block

--- README.md
+++ README.md
@@ -3,7 +3,5 @@
 ## TODO
 1. Optimize background image
 2. Refactor API calls
-3. Remove empty directories
-4. Scroller
-5. Write acceptance tests
-6. Use trending API instead of popular
+3. Scroller
+4. Write acceptance tests
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,25 @@
 import Route from '@ember/routing/route';
 import ENV from 'veriff-showcase/config/environment';
 
+function getTimeWindow(param) {
+    return param === 'week' ? 'week' : 'day';
+}
+
 export default class IndexRoute extends Route {
-    async model() {
-        const popular = await fetch(`${ENV.TMBD_API_BASE_URL}/movie/popular?api_key=${ENV.TMDB_KEY}`);
-        const top = await fetch(`${ENV.TMBD_API_BASE_URL}/movie/top_rated?api_key=${ENV.TMDB_KEY}`);
-        const popularParsed = await popular.json();
-        const topParsed = await top.json();
-        return {
-            popular: popularParsed.results,
-            top: topParsed.results
+    queryParams = {
+        timeWindow: {
+            refreshModel: true
         }
+    };
+
+    async model(params) {
+        const timeWindow = getTimeWindow(params.timeWindow);
+        const trending = await fetch(`${ENV.TMBD_API_BASE_URL}/trending/movie/${timeWindow}?api_key=${ENV.TMDB_KEY}`);
+        const trendingParsed = await trending.json();
+
+        return {
+            trending: trendingParsed.results,
+            isWeek: timeWindow === 'week'
+        };
     }
 }
